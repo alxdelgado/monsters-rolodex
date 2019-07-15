1d54ade2d69e1b43a1d437a5f4d19088655d34d9
Alex Delgado <delgadoalex@icloud.com>
2019-07-15 14:23:56 -0500

    Updates

---
+++ precache-manifest.f38f3dcdf85197dc7ae58eec6b57e175.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1462696b9fc344fdbaffb05324f524be",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5c47bb425aca3741aadf",
+    "url": "/monsters-rolodex/static/css/main.00fd54b6.chunk.css"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/monsters-rolodex/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "5c47bb425aca3741aadf",
+    "url": "/monsters-rolodex/static/js/main.bee3d7fa.chunk.js"
+  },
+  {
+    "revision": "49006584ddd3bfbd8db8",
+    "url": "/monsters-rolodex/static/js/runtime~main.26edc6c9.js"
+  }
+]);
